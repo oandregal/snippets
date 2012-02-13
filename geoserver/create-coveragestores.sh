df2fdb36ebd006fb33571216465575b523d568e0
Andrés Maneiro <amaneiro@icarto.es>
2012-02-13 13:40:34 +0100

    Allow setting directory and geoserver URI

--- geoserver/create-coveragestores.sh
+++ geoserver/create-coveragestores.sh
@@ -9,36 +9,39 @@
 # http://jira.codehaus.org/secure/attachment/49082/document_url_and_external_coverage_uploads.patch
 # http://docs.geoserver.org/stable/en/user/restconfig/index.html
 
-user=${1}
-passwd=${2}
-workspace=${3}
-#store='name'
-#file='name.tif'
-#layer='name'
+geoserver_uri=${1}
+user=${2}
+passwd=${3}
+workspace=${4}
+directory=${5}
 
-for i in `ls *.tif`;
+for i in `ls ${directory}*.tif`;
 do
 
  file=$i
  store=`basename $i .tif`
  layer=`basename $i .tif`
 
+ echo " - Store / Layer $store"
+ #echo "   Layer $layer"
+ echo "   File $file"
+
  # CREATE COVERAGESTORE
- curl -u $user:$passwd -v -XPOST -H 'Content-Type: application/xml' -d '<coverageStore><name>'$store'</name><workspace>'$workspace'</workspace></coverageStore>' http://localhost:8000/geoserver/rest/workspaces/$workspace/coveragestores
- echo "---------------- COVERAGESTORE CREATED"
+ curl -u $user:$passwd -v -XPOST -H 'Content-Type: application/xml' -d '<coverageStore><name>'$store'</name><workspace>'$workspace'</workspace></coverageStore>' ${geoserver_uri}/rest/workspaces/$workspace/coveragestores
+ echo "   * CoverageStore created: $store"
 
  # ADD LAYER TO COVERAGESTORE
- curl -u $user:$passwd -v -XPUT -H 'Content-Type: text/plain' -d 'file:/system/path/to/file/'$file http://localhost:8000/geoserver/rest/workspaces/$workspace/coveragestores/$store/external.geotiff?configure=first\&coverageName=$store
- echo "---------------- LAYER ADDED TO COVERAGESTORE"
+ curl -u $user:$passwd -v -XPUT -H 'Content-Type: text/plain' -d 'file:'${file} ${geoserver_uri}/rest/workspaces/$workspace/coveragestores/$store/external.geotiff?configure=first\&coverageName=$store
+ echo "   * Layer added to coveragestore"
 
  # ENABLE LAYER
  # it's not possible to set SRS from REST, so enable the layer is wasteful without that. At least, with the layer I've tested, which geoserver didn't recognize the SRS.
- #curl -u $user:$passwd -v -XPUT -H 'Content-Type: text/xml' -d '<layer><enabled>true</enabled></layer>' localhost:8000/geoserver/rest/layers/$layer
- #echo "---------------- LAYER ENABLED"
+ curl -u $user:$passwd -v -XPUT -H 'Content-Type: text/xml' -d '<layer><enabled>true</enabled></layer>' ${geoserver_uri}/rest/layers/$layer
+ echo "   * Layer enabled"
 
  # ENABLE COVERAGE
  # after adding the layer, and if it has right SRS, it's possible to enable the store
- curl -u $user:$passwd -v -XPUT -d '<coverageStore><name>'$store'</name><enabled>true</enabled></coverageStore>' -H "Content-Type: text/xml" http://localhost:8000/geoserver/rest/workspaces/$workspace/coveragestores/$store.xml
- echo "---------------- COVERAGESTORE ENABLED"
+ curl -u $user:$passwd -v -XPUT -d '<coverageStore><name>'$store'</name><enabled>true</enabled></coverageStore>' -H "Content-Type: text/xml" ${geoserver_uri}/rest/workspaces/$workspace/coveragestores/$store.xml
+ echo "   * CoverageStore enabled"
 
 done
