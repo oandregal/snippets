2889ae4abdb30f5c22981641abbf06ef7fc84ad0
Andrés Maneiro <amaneiro@icarto.es>
2012-06-21 10:41:08 +0200

    Added script to create a gvSIG workspace from scratch

---
+++ gvsig/create-workspace.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+projects=(addons-manager andami appgvSIG binaries \
+    extCAD extJCRS extJDBC extScripting extSymbology \
+    extWMS libCorePlugin libDriverManager libExceptions \
+    libFMap libGDBMS libInternationalization libIverUtiles \
+    libJCRS libProjection libUIComponent)
+
+rm -rf /tmp/workspace-from-scratch/
+mkdir /tmp/workspace-from-scratch
+cd /tmp/workspace-from-scratch/
+git init
+echo "Base workspace" > README
+git add README
+git ci -m "Initial commit"
+
+for p in "${projects[@]}"; do
+
+    lowercase_p=`echo ${p,,}`
+    local_remote="gvsig-${lowercase_p}"
+    local_branch="br-${local_remote}"
+
+    if [ $p = "addons-manager" ]; then
+        local_dir="org.gvsig.installer.app.extension"
+    elif [ $p = "andami" ]; then
+        local_dir="_fwAndami"
+    else
+        local_dir=$p
+    fi
+
+    echo "- remote: $local_remote"
+    echo "  branch: $local_branch"
+    echo "  dir:    $local_dir"
+
+    git remote add $local_remote git://gitorious.org/gvsig-desktop/${p}.git
+    git fetch $local_remote
+    git checkout -b $local_branch ${local_remote}/master
+    git checkout master
+    git read-tree --prefix=$local_dir -u $local_branch
+
+done
+
+git ci -m "gvSIG workspace done!"
